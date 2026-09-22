f298df9cb9f0b58e3a924ca8b7c79a24333a2623
Abhishek-Ryzen-Storm <aviseqkr@gmail.com>
2026-09-22 18:47:30 +0530

    u-boot: automate and autoboot uboot using bootcommand

--- scripts/build-uboot.sh
+++ scripts/build-uboot.sh
@@ -17,15 +17,32 @@ CROSS_COMPILE="aarch64-none-linux-gnu-"
 
 mkdir -p "$OUTPUT_DIR"
 
-cd "$ROOT/u-boot"
+export KCONFIG_CONFIG="$OUTPUT_DIR/.config"
 
-make -j"$JOBS" \
+make -C "$ROOT/u-boot" \
+	-j"$JOBS" \
 	O="$OUTPUT_DIR" \
 	ARCH="$ARCH" \
 	CROSS_COMPILE="$CROSS_COMPILE" \
 	"$DEFCONFIG"
 
-make -j"$JOBS" \
+
+# adding config values to u-boot's own compile-time env-variables for boot-automation
+"$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" --set-str BOOTCOMMAND 'booti ${kernel_addr_r} ${ramdisk_addr_r}:0x400000 ${fdt_addr_r}'
+"$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" --set-val BOOTDELAY 0
+
+# ramdisk_size is a env variable we created for our rootfs size, but instead of a precise size, assigning a fixed but big upper bound to it.
+# TODO: figure out a way to provide this to U-Boot as env value "$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" --set-str EXTRA_ENV_SETTINGS 'ramdisk_size=0x400000'
+
+make -C "$ROOT/u-boot" \
+	-j"$JOBS" \
+	O="$OUTPUT_DIR" \
+	ARCH="$ARCH" \
+	CROSS_COMPILE="$CROSS_COMPILE" \
+	olddefconfig
+
+make -C "$ROOT/u-boot" \
+	-j"$JOBS" \
 	O="$OUTPUT_DIR" \
 	ARCH="$ARCH" \
 	CROSS_COMPILE="$CROSS_COMPILE"
