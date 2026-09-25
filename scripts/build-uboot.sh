017aa4df40a067e3d8dd792317752a0b0a749e16
Abhishek-Ryzen-Storm <aviseqkr@gmail.com>
2026-09-25 05:57:45 +0530

    project: host linux boot adjustements

--- scripts/build-uboot.sh
+++ scripts/build-uboot.sh
@@ -17,8 +17,6 @@ CROSS_COMPILE="aarch64-none-linux-gnu-"
 
 mkdir -p "$OUTPUT_DIR"
 
-export KCONFIG_CONFIG="$OUTPUT_DIR/.config"
-
 make -C "$ROOT/u-boot" \
 	-j"$JOBS" \
 	O="$OUTPUT_DIR" \
@@ -28,18 +26,24 @@ make -C "$ROOT/u-boot" \
 
 
 # adding config values to u-boot's own compile-time env-variables for boot-automation
-"$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" --set-str BOOTCOMMAND 'booti ${kernel_addr_r} ${ramdisk_addr_r}:0x400000 ${fdt_addr_r}'
+"$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" --set-str BOOTCOMMAND 'booti ${kernel_addr_r} ${ramdisk_addr_r}:0x1ee448 ${fdt_addr_r}'
 "$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" --set-val BOOTDELAY 0
+
+#"$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" \
+#	--enable USE_BOOTARGS
+
+#"$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" \
+#	--set-str BOOTARGS 'kvm-arm.mode=none'
 
 # ramdisk_size is a env variable we created for our rootfs size, but instead of a precise size, assigning a fixed but big upper bound to it.
 # TODO: figure out a way to provide this to U-Boot as env value "$ROOT/u-boot/scripts/config" --file "$OUTPUT_DIR/.config" --set-str EXTRA_ENV_SETTINGS 'ramdisk_size=0x400000'
 
-make -C "$ROOT/u-boot" \
-	-j"$JOBS" \
-	O="$OUTPUT_DIR" \
-	ARCH="$ARCH" \
-	CROSS_COMPILE="$CROSS_COMPILE" \
-	olddefconfig
+#make -C "$ROOT/u-boot" \
+#	-j"$JOBS" \
+#	O="$OUTPUT_DIR" \
+#	ARCH="$ARCH" \
+#	CROSS_COMPILE="$CROSS_COMPILE" \
+#	olddefconfig
 
 make -C "$ROOT/u-boot" \
 	-j"$JOBS" \
